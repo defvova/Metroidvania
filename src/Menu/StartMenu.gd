cb5511f0f098e71e98a8f1cd830798d404ac4b6f
Vova <volodimir.partytskyi@gmail.com>
2020-06-17 23:21:31 +0300

    Add start menu

---
+++ src/Menu/StartMenu.gd
@@ -0,0 +1,14 @@
+extends Control
+
+func _ready() -> void:
+	VisualServer.set_default_clear_color(Color.black)
+
+func _on_StartButton_pressed() -> void:
+# warning-ignore:return_value_discarded
+	get_tree().change_scene("res://src/World/Level.tscn")
+
+func _on_LoadButton_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_QuitButton_pressed() -> void:
+	get_tree().quit()
