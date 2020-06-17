5aee212966927f48d9d72f030bb7fa47fd958217
Vova <volodimir.partytskyi@gmail.com>
2020-06-17 22:08:46 +0300

    Add screen shake & Events Singleton

--- src/Player/PlayerStats.gd
+++ src/Player/PlayerStats.gd
@@ -8,6 +8,8 @@ var health: int = MAX_HEALTH setget set_health
 signal player_died
 
 func set_health(value: int) -> void:
+	if value < health:
+		Events.emit_signal("add_screenshake", 0.4, 0.5)
 	health = clamp(value, 0, MAX_HEALTH) as int
 	if health == 0:
 		emit_signal("player_died")
---
+++ src/World/Camera.gd
@@ -0,0 +1,24 @@
+extends Camera2D
+
+var shake: float = 0
+
+onready var timer = $Timer as Timer
+
+func _ready() -> void:
+# warning-ignore:return_value_discarded
+	Events.connect("add_screenshake", self, "_on_Events_add_screenshake")
+
+func _process(_delta: float) -> void:
+	offset_h = rand_range(-shake, shake)
+	offset_v = rand_range(-shake, shake)
+	
+func screenshake(amount: float, duration: float) -> void:
+	shake = amount
+	timer.wait_time = duration
+	timer.start()
+	
+func _on_Timer_timeout() -> void:
+	shake = 0
+
+func _on_Events_add_screenshake(amount: float, duration: float) -> void:
+	screenshake(amount, duration)
