5380600c8f183fd12c4c8261d94c5e9725fb6dc1
Vova <volodimir.partytskyi@gmail.com>
2020-06-19 20:45:22 +0300

    Add death effect to enemy

--- src/Enemies/Enemy.gd
+++ src/Enemies/Enemy.gd
@@ -1,5 +1,7 @@
 extends KinematicBody2D
 
+const EnemyDeathEffect = preload("res://src/Effects/EnemyDeathEffect.tscn")
+
 export (int) var MAX_SPEED = 15
 
 var motion := Vector2.ZERO as Vector2
@@ -10,4 +12,6 @@ func _on_Hurtbox_hit(damage: int) -> void:
 	stats.health -= damage
 
 func _on_EnemyStats_enemy_died() -> void:
+# warning-ignore:return_value_discarded
+	Utils.instance_scene_on_main(EnemyDeathEffect, global_position)
 	queue_free()
