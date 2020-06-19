bcee6af6710e2dc57df1c3049ba758d3bb241dd6
Vova <volodimir.partytskyi@gmail.com>
2020-06-19 20:34:50 +0300

    Add Flying enemy

---
+++ src/Enemies/FlyingEnemy.gd
@@ -0,0 +1,19 @@
+extends "res://src/Enemies/Enemy.gd"
+
+export (int) var ACCELERATION = 100
+
+var MainInstances = ResourceLoader.MainInstances
+
+onready var sprite := $Sprite as Sprite
+
+func _physics_process(delta: float) -> void:
+	var player: KinematicBody2D = MainInstances.Player
+	if player:
+		chase_player(player, delta)
+		
+func chase_player(player: KinematicBody2D, delta: float) -> void:
+	var direction = (player.global_position - global_position).normalized()
+	motion += direction * ACCELERATION * delta
+	motion = motion.clamped(MAX_SPEED)
+	sprite.flip_h = global_position < player.global_position
+	motion = move_and_slide(motion)
