2dcaa87163c44d8f63ad67abcc24eadaccba2fb3
Vova <volodimir.partytskyi@gmail.com>
2020-06-20 17:04:30 +0300

    Add ability to fire missile

--- src/Player/Player.gd
+++ src/Player/Player.gd
@@ -4,6 +4,7 @@ const DustEffect = preload("res://src/Effects/DustEffect.tscn")
 const JumpEffect = preload("res://src/Effects/JumpEffect.tscn")
 const PlayerBullet = preload("res://src/Player/PlayerBullet.tscn")
 const WallDustEffect = preload("res://src/Effects/WallDustEffect.tscn")
+const PlayerMissile = preload("res://src/Player/PlayerMissile.tscn")
 
 var PlayerStats = ResourceLoader.PlayerStats
 var MainInstances = ResourceLoader.MainInstances
@@ -17,6 +18,7 @@ export (int) var MAX_WALL_SLIDE_SPEED = 128
 export (int) var JUMP_FORCE = 128
 export (int) var MAX_SLOPE_ANGLE = 46
 export (int) var BULLET_SPEED = 250
+export (int) var MISSILE_BULLET_SPEED = 150
 
 enum {
 	MOVE,
@@ -75,12 +77,23 @@ func _physics_process(delta: float) -> void:
 
 	if Input.is_action_pressed("fire") && fireBulletTimer.time_left == 0:
 		fire_bullet()
+		
+	if Input.is_action_pressed("fire_missile") && fireBulletTimer.time_left == 0:
+		fire_missile()
 
 func fire_bullet() -> void:
 	var bullet: Object = Utils.instance_scene_on_main(PlayerBullet, muzzle.global_position)
 	bullet.velocity = Vector2.RIGHT.rotated(gun.rotation) * BULLET_SPEED
 	bullet.velocity.x *= sprite.scale.x
 	bullet.rotation = bullet.velocity.angle()
+	fireBulletTimer.start()
+	
+func fire_missile() -> void:
+	var missile: Object = Utils.instance_scene_on_main(PlayerMissile, muzzle.global_position)
+	missile.velocity = Vector2.RIGHT.rotated(gun.rotation) * MISSILE_BULLET_SPEED
+	missile.velocity.x *= sprite.scale.x
+	motion -= missile.velocity
+	missile.rotation = missile.velocity.angle()
 	fireBulletTimer.start()
 
 func create_dust_effect() -> void:
@@ -135,7 +148,11 @@ func apply_gravity(delta: float) -> void:
 		motion.y = min(motion.y, JUMP_FORCE)
 
 func update_animation(input_vector: Vector2) -> void:
-	sprite.scale.x = sign(get_local_mouse_position().x)
+	var facing: float = sign(get_local_mouse_position().x)
+	
+	if facing != 0:
+		sprite.scale.x = facing 
+		
 	if input_vector.x == 0:
 		spriteAnimator.play("Idle")
 	else:
